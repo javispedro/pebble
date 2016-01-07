b53f1e24610d2a37ff65a2a5d4575a6f6ed29bcf
Javier <dev.git@javispedro.com>
2016-01-08 00:01:31 +0100

    implement packing of timeline messages

--- daemon/timelinemanager.cc
+++ daemon/timelinemanager.cc
@@ -1,7 +1,62 @@
 #include "timelinemanager.h"
+#include "packer.h"
 
 TimelineManager::TimelineManager(WatchConnector *watch, BlobDbManager *blobdb, QObject *parent)
     : QObject(parent), l(metaObject()->className()), watch(watch), blobdb(blobdb)
 {
 }
 
+void TimelineManager::publish(const Item &item)
+{
+    QByteArray data;
+    Packer p(&data);
+
+    p.writeUuid(item.id);
+    p.writeUuid(item.parent);
+    p.writeLE<quint32>(item.dateTime.toTime_t());
+    p.writeLE<quint16>(item.duration);
+    p.writeLE<quint8>(item.type);
+    p.writeLE<quint16>(item.flags);
+    p.writeLE<quint8>(item.layout);
+
+    QByteArray attributes = packAttributes(item.attributes);
+    QByteArray actions = packActions(item.actions);
+
+    p.writeLE<quint16>(attributes.size() + actions.size());
+    p.writeLE<quint8>(item.attributes.size());
+    p.writeLE<quint8>(item.actions.size());
+
+    data.append(attributes);
+    data.append(actions);
+
+    blobdb->insert(BlobDbManager::BlobDbNotification, item.id, data);
+}
+
+QByteArray TimelineManager::packAttributes(const QList<Attribute>& attributes)
+{
+    QByteArray data;
+    Packer p(&data);
+
+    foreach (const Attribute &attr, attributes) {
+        p.writeLE<quint8>(attr.type);
+        p.writeLE<quint16>(attr.content.size());
+        data.append(attr.content);
+    }
+
+    return data;
+}
+
+QByteArray TimelineManager::packActions(const QList<Action>& actions)
+{
+    QByteArray data;
+    Packer p(&data);
+
+    foreach (const Action &action, actions) {
+        p.writeLE<quint8>(action.id);
+        p.writeLE<quint8>(action.type);
+        p.writeLE<quint8>(action.attributes.size());
+        data.append(packAttributes(action.attributes));
+    }
+
+    return data;
+}
--- daemon/timelinemanager.h
+++ daemon/timelinemanager.h
@@ -11,9 +11,56 @@ class TimelineManager : public QObject
 public:
     explicit TimelineManager(WatchConnector *watch, BlobDbManager *blobdb, QObject *parent = 0);
 
-signals:
+    enum ItemType {
+        TimelineNotification = 1,
+        TimelinePin = 2,
+        TimelineReminder = 3
+    };
+
+    enum AttributeType {
+        AttributeSender = 1,
+        AttributeSubject = 2,
+        AttributeMessage = 3,
+        AttributeSource = 4
+    };
+
+    enum NotificationSource {
+        SourceUnknown = 1,
+        SourceEmail = 19,
+        SourceFacebook = 11,
+        SourceSMS = 45,
+        SourceTwitter = 6
+    };
+
+    struct Attribute {
+        AttributeType type;
+        QByteArray content;
+    };
+
+    struct Action {
+        quint8 id;
+        quint8 type; // TODO
+        QList<Attribute> attributes;
+    };
+
+    struct Item {
+        QUuid id;
+        QUuid parent;
+        QDateTime dateTime;
+        unsigned short duration;
+        ItemType type;
+        quint16 flags;
+        quint8 layout;
+        QList<Attribute> attributes;
+        QList<Action> actions;
+    };
 
 public slots:
+    void publish(const Item &item);
+
+private:
+    static QByteArray packAttributes(const QList<Attribute>& attributes);
+    static QByteArray packActions(const QList<Action>& actions);
 
 private:
     QLoggingCategory l;
